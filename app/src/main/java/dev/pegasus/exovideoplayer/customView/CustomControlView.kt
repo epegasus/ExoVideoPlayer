1a6dd41e3acb8e851d7c70f4850b8c3d23b5283e
Sohaib Ahmed <sohaibmughal93@gmail.com>
2023-12-04 22:11:55 +0500

    ExoVideoPlayer

---
+++ app/src/main/java/dev/pegasus/exovideoplayer/customView/CustomControlView.kt
@@ -0,0 +1,24 @@
+package dev.pegasus.exovideoplayer.customView
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.media3.ui.PlayerControlView
+
+/**
+ * @Author: SOHAIB AHMED
+ * @Date: 04,December,2023.
+ * @Accounts
+ *      -> https://github.com/epegasus
+ *      -> https://linkedin.com/in/epegasus
+ */
+
+@androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
+class CustomControlView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : PlayerControlView(context, attrs, defStyleAttr) {
+
+    // You can customize the control view further if needed
+
+}
