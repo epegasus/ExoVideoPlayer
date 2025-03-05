551e1a0287cad5d073785391d630f5e3f1f83f89
Sohaib Ahmed <sohaibahmed.hypersoft@gmail.com>
2025-03-05 10:20:46 +0500

    - Improved ExoPlayer (m3u8)

--- app/src/main/java/dev/pegasus/exovideoplayer/m3u8Player/ActivityM3u8Player.kt
+++ app/src/main/java/dev/pegasus/exovideoplayer/m3u8Player/ActivityM3u8Player.kt
@@ -2,18 +2,14 @@ package dev.pegasus.exovideoplayer.m3u8Player
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import androidx.media3.common.MediaItem
 import androidx.media3.common.PlaybackException
 import androidx.media3.common.Player
-import androidx.media3.common.util.UnstableApi
-import androidx.media3.common.util.Util
-import androidx.media3.datasource.DefaultDataSourceFactory
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.hls.HlsMediaSource
 import dev.pegasus.exovideoplayer.R
 import dev.pegasus.exovideoplayer.databinding.ActivityM3u8PlayerBinding
 
@@ -41,50 +37,35 @@ class ActivityM3u8Player : AppCompatActivity() {
         }
     }
 
-    @OptIn(UnstableApi::class)
-    /*private fun initPlayerM3U8() {
-        binding.playerView.player = exoPlayer
-
-        val videoUri = "https://vod3.cf.dmcdn.net/sec2(xN82Wovbx_0A1BkRYKI_sL3_F00V5oY2gs96oikU2mA_80X1N1mFw9NL0tN5xC3L4SLit_e1hyx2dRrWMGQ4q_TisPyRO6Fd9QT6_UF42sYqa3bnf85t-d8apCPCwVNTiJ0R8lZSfAiVoYq6m7h9jLq9KyE5iEg5s0ElSksYpxY)/video/614/142/476241416_mp4_h264_aac.m3u8".toUri()
-        //val videoUri = "https://live-hls-abr-cdn.livepush.io/live/bigbuckbunnyclip/index.m3u8".toUri()
-
-        val mediaItem = MediaItem.fromUri(videoUri)
-        val mediaSource = HlsMediaSource
-            .Factory(DefaultDataSourceFactory(this, Util.getUserAgent(this, "app-name")))
-            .createMediaSource(mediaItem)
-
-        exoPlayer.setMediaSource(mediaSource)
-        exoPlayer.prepare()
-        exoPlayer.playWhenReady = true
-    }*/
+    /**
+     *  There are two types of Videos in m3u8,
+     *      1) VOD (video on demand)
+     *      2) Live Streaming
+     */
 
     private fun initPlayerM3U8() {
-        binding.playerView.player = exoPlayer
+        //val videoUrl = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"        // VOD
+        val videoUrl = "https://live-hls-web-aje.getaj.net/AJE/index.m3u8"          // LIVE
 
-        // Replace this with your actual URL (Live or VOD)
-        //val videoUrl = "https://vod3.cf.dmcdn.net/sec2(xN82Wovbx_0A1BkRYKI_sL3_F00V5oY2gs96oikU2mA_80X1N1mFw9NL0tN5xC3L4SLit_e1hyx2dRrWMGQ4q_TisPyRO6Fd9QT6_UF42sYqa3bnf85t-d8apCPCwVNTiJ0R8lZSfAiVoYq6m7h9jLq9KyE5iEg5s0ElSksYpxY)/video/614/142/476241416_mp4_h264_aac.m3u8"
-        val videoUrl = "https://live-hls-web-aje.getaj.net/AJE/index.m3u8"
+        val mediaItem = MediaItem.fromUri(videoUrl)
+        exoPlayer.setMediaItem(mediaItem)
 
-        val mediaItem = MediaItem.Builder()
-            .setUri(videoUrl)
-            .setLiveConfiguration(MediaItem.LiveConfiguration.Builder().setMaxPlaybackSpeed(1.02f).build()) // Helps with live streams
-            .build()
-
-        val mediaSource = HlsMediaSource
-            .Factory(DefaultDataSourceFactory(this, Util.getUserAgent(this, "app-name")))
-            .createMediaSource(mediaItem)
-
-        exoPlayer.setMediaSource(mediaSource)
         exoPlayer.prepare()
         exoPlayer.playWhenReady = true
 
         // ⚡ Auto-reconnect if live stream fails
         exoPlayer.addListener(object : Player.Listener {
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                binding.progressBar.isVisible = playbackState == ExoPlayer.STATE_BUFFERING
+            }
+
             override fun onPlayerError(error: PlaybackException) {
                 exoPlayer.seekToDefaultPosition() // Restart from the latest position
                 exoPlayer.prepare()
             }
         })
+
+        binding.playerView.player = exoPlayer
     }
 
     override fun onPause() {
